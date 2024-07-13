4ca955f35c3d45742539ce6ab3e968ac88c22cd8
satyendra kumar <satyendra1es1ds@gmail.com>
2024-07-13 19:29:50 +0530

    first commit

---
+++ src/Components/HomePage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import '../Styles/LandingPage.css';
+const HomePage = () =>{
+
+    return(
+        <div className="header">
+            <h1>Welcome to My Project</h1>
+        </div>
+    )
+}
+export default HomePage;
